ecc76d4165fe021b41225476cf87063e763823c5
Darren888Le <darrenle186@gmail.com>
2024-07-05 22:20:46 +1000

    sad and happy and process images

--- catdog.py
+++ catdog.py
@@ -1,1 +1,31 @@
-hello
+import tensorflow as tf
+import os
+import cv2
+import imghdr
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+# gpus = tf.config.experimental.list_physical_devices('CPU')
+# for gpu in gpus:
+#     tf.config.experimental.set_memory_growth(gpu, True)
+
+data_dir = 'data'
+image_exts = ['jpeg','jpg', 'bmp', 'png']
+
+for image_class in os.listdir(data_dir): 
+    for image in os.listdir(os.path.join(data_dir, image_class)):
+        image_path = os.path.join(data_dir, image_class, image)
+        try: 
+            img = cv2.imread(image_path) 
+            tip = imghdr.what(image_path)
+            if tip not in image_exts: 
+                print('Image not in ext list {}'.format(image_path))
+                os.remove(image_path)
+        except Exception as e: 
+            print('Issue with image {}'.format(image_path))
+            # os.remove(image_path)
+
+data = tf.keras.utils.image_dataset_from_directory('data')
+data_iterator = data.as_numpy_iterator()   
+
